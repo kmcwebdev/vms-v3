c88879cbd0f7b66e9df05fdaba425715e477ff9c
Seth Maño <joshuaseth11@gmail.com>
2023-11-09 11:34:07 +0800

    Added Dashboard page

---
+++ app/(admin)/cms/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CmsAdmin from "@/components/admin/CmsAdmin";
+
+const page = () => {
+  return (
+    <div>
+      <CmsAdmin />
+    </div>
+  );
+};
+
+export default page;
---
+++ app/(admin)/layout.tsx
@@ -0,0 +1,5 @@
+const layout = ({ children }: { children: React.ReactNode }) => {
+  return <main className="h-screen p-6 sm:p-12">{children}</main>;
+};
+
+export default layout;
