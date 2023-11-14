e7ce74c34f51cf463a5cc771c2fa9dee7a35a027
Seth Maño <joshuaseth11@gmail.com>
2023-11-14 14:01:18 +0800

    style: Improve UI layout and add tabs for site details

--- components/admin/cms/area-sites/site-details.tsx
+++ components/admin/cms/area-sites/site-details.tsx
@@ -1,14 +1,59 @@
+"use client";
+
 import React from "react";
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
-import Image from "next/image";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Badge } from "@/components/ui/badge";
+import { Copy } from "lucide-react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { TabsContent } from "@radix-ui/react-tabs";
 
 const SiteDetails = () => {
   return (
     <Card className="shadow-none">
       <CardHeader>
-        <div className="h-64 w-full rounded-md bg-gray-100" />
+        <CardTitle className="mb-2 flex items-center justify-between">
+          <p>Armstrong Corporate Center</p>
+          <TooltipProvider>
+            <Tooltip>
+              <Badge
+                variant="secondary"
+                className=" w-1/2 space-x-2 py-2 text-sm font-normal text-gray-700"
+              >
+                <TooltipTrigger asChild>
+                  <Copy className="h-5 w-5 cursor-pointer text-primary " />
+                </TooltipTrigger>
+                <p className="truncate">
+                  https://visitor-management.kmc.solutions/tablet?tabletConfigId=cld9xhppq0001pevzvegwc8ou
+                </p>
+              </Badge>
+              <TooltipContent>
+                <p>Copy tablet link</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </CardTitle>
+        <div className=" h-96 w-full rounded-md bg-gray-100" />
       </CardHeader>
-      <CardContent></CardContent>
+      <CardContent>
+        <div className="flex ">
+          <Tabs defaultValue="overview">
+            <TabsList>
+              <TabsTrigger value="overview">Overview</TabsTrigger>
+              <TabsTrigger value="clients">Clients</TabsTrigger>
+              <TabsTrigger value="visitors">Visitors</TabsTrigger>
+            </TabsList>
+            <TabsContent value="overview">
+              <p>faf</p>
+            </TabsContent>
+          </Tabs>
+        </div>
+      </CardContent>
     </Card>
   );
 };
