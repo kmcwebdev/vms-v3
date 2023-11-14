61af7893b554be51ad055b6b549622d72311df8f
Seth Maño <joshuaseth11@gmail.com>
2023-11-14 16:47:53 +0800

    style: remove unnecessary div wrapper

--- components/admin/index.tsx
+++ components/admin/index.tsx
@@ -11,50 +11,46 @@ const Admin = () => {
   const searchParams = useSearchParams();
 
   return (
-    <div>
-      <Tabs defaultValue={searchParams.get("tab") || "dashboard"}>
-        <TabsList>
-          <TabsTrigger
-            value="dashboard"
-            onClick={() =>
-              router.push(`${window.location.pathname}?tab=dashboard`)
-            }
-          >
-            Dashboard
-          </TabsTrigger>
-          <TabsTrigger
-            value="analytics"
-            onClick={() =>
-              router.push(`${window.location.pathname}?tab=analytics`)
-            }
-          >
-            Analytics
-          </TabsTrigger>
-          <TabsTrigger
-            value="reports"
-            onClick={() =>
-              router.push(`${window.location.pathname}?tab=reports`)
-            }
-          >
-            Reports
-          </TabsTrigger>
-          <TabsTrigger
-            value="notifications"
-            onClick={() =>
-              router.push(`${window.location.pathname}?tab=notifications`)
-            }
-          >
-            Notifications
-          </TabsTrigger>
-        </TabsList>
-        <TabsContent value="dashboard">
-          <Dashboard />
-        </TabsContent>
-        <TabsContent value="analytics">
-          <Analytics />
-        </TabsContent>
-      </Tabs>
-    </div>
+    <Tabs defaultValue={searchParams.get("tab") || "dashboard"}>
+      <TabsList>
+        <TabsTrigger
+          value="dashboard"
+          onClick={() =>
+            router.push(`${window.location.pathname}?tab=dashboard`)
+          }
+        >
+          Dashboard
+        </TabsTrigger>
+        <TabsTrigger
+          value="analytics"
+          onClick={() =>
+            router.push(`${window.location.pathname}?tab=analytics`)
+          }
+        >
+          Analytics
+        </TabsTrigger>
+        <TabsTrigger
+          value="reports"
+          onClick={() => router.push(`${window.location.pathname}?tab=reports`)}
+        >
+          Reports
+        </TabsTrigger>
+        <TabsTrigger
+          value="notifications"
+          onClick={() =>
+            router.push(`${window.location.pathname}?tab=notifications`)
+          }
+        >
+          Notifications
+        </TabsTrigger>
+      </TabsList>
+      <TabsContent value="dashboard">
+        <Dashboard />
+      </TabsContent>
+      <TabsContent value="analytics">
+        <Analytics />
+      </TabsContent>
+    </Tabs>
   );
 };
 
