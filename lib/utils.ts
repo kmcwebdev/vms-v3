2a0d580e307ccbe6c93364eb7ee71258391055e5
Seth Maño <joshuaseth11@gmail.com>
2023-11-15 13:56:22 +0800

    feat(utils): add createSearchParams function

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,22 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function createSearchParams(queryParams: Record<string, any>) {
+  const isValidJSONObject =
+    queryParams !== null &&
+    typeof queryParams === "object" &&
+    !Array.isArray(queryParams);
+
+  if (isValidJSONObject) {
+    const searchParams = new URLSearchParams();
+
+    for (const [key, value] of Object.entries(queryParams)) {
+      if (value !== undefined && value !== "" && value !== null) {
+        searchParams.append(key, String(value));
+      }
+    }
+
+    return searchParams;
+  }
+}
