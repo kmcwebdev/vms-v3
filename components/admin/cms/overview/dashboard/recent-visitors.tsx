f5cd3f4ef3afb057dc0b79bb63d0cf9cd83db13b
Seth Maño <joshuaseth11@gmail.com>
2023-11-15 16:33:37 +0800

    feat(reports): add visitor data table

--- components/admin/cms/overview/dashboard/recent-visitors.tsx
+++ components/admin/cms/overview/dashboard/recent-visitors.tsx
@@ -92,64 +92,6 @@ export const visitorData: IRecentVisitors[] = [
   },
 ];
 
-// export const columns: ColumnDef<IRecentVisitors>[] = [
-//   {
-//     accessorKey: "timeout",
-//     header: "Time out details",
-//     cell: ({ row }) => (
-//       <Badge className="capitalize">{row.getValue("timeout")}</Badge>
-//     ),
-//   },
-//   {
-//     accessorKey: "name",
-//     header: "Name",
-
-//     cell: ({ row }) => <div className="capitalize">{row.getValue("name")}</div>,
-//   },
-//   {
-//     accessorKey: "siteVisited",
-//     header: "Site",
-//     cell: ({ row }) => (
-//       <div className="capitalize">{row.getValue("siteVisited")}</div>
-//     ),
-//   },
-//   {
-//     accessorKey: "reasonToVisit",
-//     header: "Reason",
-//     cell: ({ row }) => (
-//       <div className="capitalize">{row.getValue("reasonToVisit")}</div>
-//     ),
-//   },
-//   {
-//     accessorKey: "companyToVisit",
-//     header: "Company ",
-//     cell: ({ row }) => (
-//       <div className="capitalize">{row.getValue("companyToVisit")}</div>
-//     ),
-//   },
-//   {
-//     accessorKey: "personToVisit",
-//     header: "Person visited ",
-//     cell: ({ row }) => (
-//       <div className="capitalize">{row.getValue("personToVisit")}</div>
-//     ),
-//   },
-//   {
-//     accessorKey: "dateVisited",
-//     header: "Date ",
-//     cell: ({ row }) => (
-//       <div className="capitalize">{row.getValue("dateVisited")}</div>
-//     ),
-//   },
-//   {
-//     accessorKey: "timeVisited",
-//     header: "Time ",
-//     cell: ({ row }) => (
-//       <div className="capitalize">{row.getValue("timeVisited")}</div>
-//     ),
-//   },
-// ];
-
 const RecentVisitors = () => {
   return (
     <Card className="w-full shadow-none">
--- components/admin/cms/overview/reports.tsx
+++ components/admin/cms/overview/reports.tsx
@@ -1,9 +1,87 @@
 import React from "react";
 import DataTable from "@/components/global/data-table";
-import { Card } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import type { ColumnDef } from "@tanstack/react-table";
+import { type IRecentVisitors, visitorData } from "./dashboard/recent-visitors";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
 
 const Reports = () => {
-  return <Card className="shadow-none"></Card>;
+  const columns: ColumnDef<IRecentVisitors>[] = [
+    {
+      accessorKey: "timeout",
+      header: "Time out",
+      cell: ({ row }) => (
+        <Badge className="truncate text-xs capitalize">
+          {row.getValue("timeout")}
+        </Badge>
+      ),
+    },
+    {
+      accessorKey: "name",
+      header: "Name",
+
+      cell: ({ row }) => (
+        <div className="capitalize">{row.getValue("name")}</div>
+      ),
+    },
+    {
+      accessorKey: "siteVisited",
+      header: "Site",
+      cell: ({ row }) => (
+        <div className="capitalize">{row.getValue("siteVisited")}</div>
+      ),
+    },
+    {
+      accessorKey: "reasonToVisit",
+      header: "Reason",
+      cell: ({ row }) => (
+        <div className="capitalize">{row.getValue("reasonToVisit")}</div>
+      ),
+    },
+    {
+      accessorKey: "companyToVisit",
+      header: "Company ",
+      cell: ({ row }) => (
+        <div className="capitalize">{row.getValue("companyToVisit")}</div>
+      ),
+    },
+    {
+      accessorKey: "personToVisit",
+      header: "Person visited ",
+      cell: ({ row }) => (
+        <div className="capitalize">{row.getValue("personToVisit")}</div>
+      ),
+    },
+    {
+      accessorKey: "dateVisited",
+      header: "Date ",
+      cell: ({ row }) => (
+        <div className="capitalize">{row.getValue("dateVisited")}</div>
+      ),
+    },
+    {
+      accessorKey: "timeVisited",
+      header: "Time ",
+      cell: ({ row }) => (
+        <div className="capitalize">{row.getValue("timeVisited")}</div>
+      ),
+    },
+  ];
+
+  return (
+    <Card className=" shadow-none">
+      <CardHeader>
+        <CardTitle className="flex items-center justify-between">
+          <h1>Visitor</h1>
+          <Button>Export</Button>
+        </CardTitle>
+      </CardHeader>
+      <CardContent>
+        <DataTable data={visitorData} columns={columns} />
+      </CardContent>
+    </Card>
+  );
 };
 
 export default Reports;
