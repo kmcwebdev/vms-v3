d25e8010384d0aa54993e166b59dcef7b208514f
Seth Maño <joshuaseth11@gmail.com>
2023-11-16 16:56:34 +0800

    style: Update page component to display a construction message

--- app/(admin)/cms/manage-users/page.tsx
+++ app/(admin)/cms/manage-users/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <Card className="text-center opacity-50 shadow-none">
+      <CardHeader>Page under construction</CardHeader>
+    </Card>
+  );
 };
 
 export default page;
