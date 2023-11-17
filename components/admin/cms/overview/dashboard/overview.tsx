908227de6e9df53991ceb91468ebb8ab9727d3da
Seth Maño <joshuaseth11@gmail.com>
2023-11-17 16:50:16 +0800

    chore: Add skeleton loader for overview cards

--- components/admin/cms/overview/dashboard/overview.tsx
+++ components/admin/cms/overview/dashboard/overview.tsx
@@ -5,11 +5,14 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Users2, Building } from "lucide-react";
 import { useGetTotalVisitorOnAllSites } from "@/hooks/useGetTotalVisitorOnAllSites";
 import { useGetMostVisitedSite } from "@/hooks/useGetMostVisitedSite";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const Overview = () => {
-  const { data: totalVisitor } = useGetTotalVisitorOnAllSites();
+  const { data: totalVisitor, isLoading: isTotalVisitorLoading } =
+    useGetTotalVisitorOnAllSites();
 
-  const { data: mostVisitedSite } = useGetMostVisitedSite();
+  const { data: mostVisitedSite, isLoading: isMostVisitedSiteLoading } =
+    useGetMostVisitedSite();
 
   const overview_data = [
     {
@@ -28,20 +31,48 @@ const Overview = () => {
 
   return (
     <div className="grid grid-cols-3 grid-rows-1 gap-x-4">
-      {overview_data.map((e) => (
-        <Card key={e.title} className="shadow-none">
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">{e.title}</CardTitle>
-            <div className="text-gray-400">{e.icon}</div>
-          </CardHeader>
-          <CardContent>
-            <div className="truncate text-2xl font-bold">{e.value}</div>
-            <p className="text-xs text-muted-foreground">{e.description}</p>
-          </CardContent>
-        </Card>
-      ))}
+      {isTotalVisitorLoading || isMostVisitedSiteLoading ? (
+        <>
+          <OverviewCardSkeleton />
+          <OverviewCardSkeleton />
+        </>
+      ) : (
+        <>
+          {overview_data.map((e) => (
+            <Card key={e.title} className="shadow-none">
+              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                <CardTitle className="text-sm font-medium">{e.title}</CardTitle>
+                <div className="text-gray-400">{e.icon}</div>
+              </CardHeader>
+              <CardContent>
+                <div className="truncate text-2xl font-bold">{e.value}</div>
+                <p className="text-xs text-muted-foreground">{e.description}</p>
+              </CardContent>
+            </Card>
+          ))}
+        </>
+      )}
     </div>
   );
 };
 
 export default Overview;
+
+const OverviewCardSkeleton = () => {
+  return (
+    <Card className="shadow-none">
+      <Skeleton className="h-full w-full bg-transparent">
+        <CardHeader className="flex flex-row items-center justify-between pb-2">
+          <CardTitle>
+            <div className="h-6 w-32 rounded-lg bg-neutral-100" />
+          </CardTitle>
+          <div className="h-6 w-6 rounded-lg bg-neutral-100" />
+        </CardHeader>
+        <CardContent>
+          <div className="h-6 w-24 rounded-lg bg-neutral-100" />
+          <div className="mt-2 h-3 w-14 rounded-lg bg-neutral-100" />
+        </CardContent>
+      </Skeleton>
+    </Card>
+  );
+};
