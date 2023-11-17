b3c52b32ca40bedd1b7b376c33567f39fa6ce929
Seth Maño <joshuaseth11@gmail.com>
2023-11-17 15:40:33 +0800

    refactor: refactor useGetTotalVisitorOnAllSites hook

--- hooks/useGetTotalVisitorOnAllSites.ts
+++ hooks/useGetTotalVisitorOnAllSites.ts
@@ -1,19 +1,27 @@
 import { useQuery } from "@tanstack/react-query"
 
+type TotalVisitorOnAllSites = [
+    {
+        total_visitors: string
+    }
+]
+
 const getTotalVisitorOnAllSitesQuery = async () => {
     const URL = `api/analytics/visitor-count/total-visitors`
 
     const response = await fetch(URL)
 
-    return (await response.json()) as number
+    return (await response.json()) as TotalVisitorOnAllSites
 }
 
 export const useGetTotalVisitorOnAllSites = () => {
 
-    const {data, isLoading, isFetching, isError} = useQuery({
+    const {data: dataArray, isLoading, isFetching, isError} = useQuery({
         queryKey: ['total-visitor-on-all-sites'],
         queryFn: () => getTotalVisitorOnAllSitesQuery(),
     })
+
+    const data = dataArray ? dataArray[0] : null;
 
     return {
         data,
