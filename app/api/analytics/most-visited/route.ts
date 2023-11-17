11bf673867e81645657a46288fb95cd6977f5817
Seth Maño <joshuaseth11@gmail.com>
2023-11-17 16:02:36 +0800

    feat: added api route for most-visited-site

--- app/api/analytics/most-visited/route.ts
+++ app/api/analytics/most-visited/route.ts
@@ -1,5 +1,26 @@
 import { NextResponse } from "next/server";
+import { sql } from "@vercel/postgres";
 
 export async function GET() {
-  return NextResponse.json({});
+ try{
+  const most_visited_query = `
+  select s.site_name, count(*) 
+from visitor_logs vl
+join sites s on s.site_id = vl.site_id
+group by s.site_name
+order by count(*) desc
+limit 1;
+  `
+
+  const result = await sql.query(most_visited_query);
+
+  return NextResponse.json(result.rows);
+
+ }
+ catch(error: any){
+  return NextResponse.json(
+    { status: 500, message: error.message },
+    { status: 500, statusText: "Internal server error" },
+);
+ }
 }
