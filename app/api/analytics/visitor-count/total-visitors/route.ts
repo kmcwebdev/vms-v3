687b548a0cf370dbfcb241daa8172e927cc15956
Gelo <chrisgelosulit@gmail.com>
2023-11-20 14:11:40 +0800

    refactor: Simplify error handling in analytics routes

--- app/api/analytics/visitor-count/total-visitors/route.ts
+++ app/api/analytics/visitor-count/total-visitors/route.ts
@@ -2,19 +2,19 @@ import { NextResponse } from "next/server";
 import { sql } from "@vercel/postgres";
 
 export async function GET() {
-    try {
-        const total_visitors_query = `
+  try {
+    const total_visitors_query = `
             select count(*) as total_visitors
             from visitor_logs;
         `;
 
-        const result = await sql.query(total_visitors_query);
+    const result = await sql.query(total_visitors_query);
 
-        return NextResponse.json(result.rows);
-    } catch (error: any) {
-        return NextResponse.json(
-            { status: 500, message: error.message },
-            { status: 500, statusText: "Internal server error" },
-        );
-    }
+    return NextResponse.json(result.rows);
+  } catch (error: any) {
+    return NextResponse.json(
+      { status: 500, message: error.message },
+      { status: 500, statusText: "Internal server error" },
+    );
+  }
 }
