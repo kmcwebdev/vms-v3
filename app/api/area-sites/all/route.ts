007c7a35a28d2cef53bcc3f3d9c132e230218cdd
Seth Maño <joshuaseth11@gmail.com>
2023-11-20 14:12:13 +0800

    fix(api): fix search query in GET route

--- app/api/area-sites/all/route.ts
+++ app/api/area-sites/all/route.ts
@@ -1,15 +1,17 @@
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
+import type { NextApiRequest, NextApiResponse } from "next";
 import { sql } from "@vercel/postgres";
 
-export async function GET () {
+export async function GET (request:NextApiRequest, response: NextApiResponse) {
     try{
-        const all_sites_query = `SELECT site_id, site_name, site_banner, site_images, address FROM sites ORDER BY site_name`
+        const searchTerm = request.query.searchTerm;
+        const all_sites_query = `SELECT site_id, site_name, site_banner, site_images, address FROM sites WHERE site_name ILIKE $1 ORDER BY site_name`;
 
-        const result = await sql.query(all_sites_query);
+        const result = await sql.query(all_sites_query, [`%${searchTerm}%`]);
 
         return NextResponse.json(result.rows);
     }
-    catch(error: any){
+    catch(error: any){ 
         return NextResponse.json({
             status: 500, message: error.message
         },{
