2f96cf2e90d7cb4458cbf2639ddb0441cdfb25d3
Gelo <chrisgelosulit@gmail.com>
2023-11-20 14:18:30 +0800

    fix(app/api/area-sites/all/route): Fix SQL injection vulnerability

--- app/api/area-sites/all/route.ts
+++ app/api/area-sites/all/route.ts
@@ -2,21 +2,30 @@ import { NextResponse, NextRequest } from "next/server";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { sql } from "@vercel/postgres";
 
-export async function GET (request:NextApiRequest, response: NextApiResponse) {
-    try{
-        const searchTerm = request.query.searchTerm;
-        const all_sites_query = `SELECT site_id, site_name, site_banner, site_images, address FROM sites WHERE site_name ILIKE $1 ORDER BY site_name`;
+export async function GET(request: NextApiRequest, response: NextApiResponse) {
+  try {
+    const searchTerm = request.query.searchTerm;
 
-        const result = await sql.query(all_sites_query, [`%${searchTerm}%`]);
+    const all_sites_query = `
+        select site_id, site_name, site_banner, site_images, address 
+        from sites 
+        ${searchTerm ? `where site_name ilike '%${searchTerm}%'` : ``}
+        order by site_name
+    `;
 
-        return NextResponse.json(result.rows);
-    }
-    catch(error: any){ 
-        return NextResponse.json({
-            status: 500, message: error.message
-        },{
-            status: 500, statusText: "Internal server error"
-        });
-    }
+    const result = await sql.query(all_sites_query);
 
+    return NextResponse.json(result.rows);
+  } catch (error: any) {
+    return NextResponse.json(
+      {
+        status: 500,
+        message: error.message,
+      },
+      {
+        status: 500,
+        statusText: "Internal server error",
+      },
+    );
+  }
 }
