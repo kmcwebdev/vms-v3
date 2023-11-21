3e10e226e376b8cedfb0a7a7f19ee9024c10d47c
Gelo <chrisgelosulit@gmail.com>
2023-11-21 13:41:58 +0800

    refactor(route): Remove unused parameter in GET function

--- app/api/visitors/export-csv/route.ts
+++ app/api/visitors/export-csv/route.ts
@@ -1,6 +1,6 @@
 import { parse } from "json2csv";
 
-export async function GET(req: Request, res: Response) {
+export async function GET(req: Request) {
   const data = [
     { name: "John Doe", email: "john@example.com" },
     { name: "Jane Smith", email: "jane@example.com" },
