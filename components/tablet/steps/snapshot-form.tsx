e5f2baa08ad05a21500f7ed2a6f414fea7a15abf
Seth Maño <joshuaseth11@gmail.com>
2023-11-21 15:47:11 +0800

    fix(snapshot-form): fix typo in variable name

--- components/tablet/steps/snapshot-form.tsx
+++ components/tablet/steps/snapshot-form.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, useState } from "react";
 import { Camera } from "react-camera-pro";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import { count } from "console";
 
 type CameraErrorMessage = {
   noCameraAccessible?: string;
@@ -19,6 +20,10 @@ const SnapshotForm = ({
   takePhotoTrigger,
   setHasImageTaken,
 }: ISnapshotForm) => {
+  const [count, setCount] = useState<string | number>();
+
+  console.log("count", count);
+
   const camera = useRef<{ takePhoto: () => string } | null>(null);
   const [image, setImage] = useState<string | null>(null);
 
@@ -32,9 +37,27 @@ const SnapshotForm = ({
     canvas: "Canvas is not supported.",
   };
 
+  const startCountdown = () => {
+    let count = 3;
+
+    const intervalId = setInterval(() => {
+      if (count > 1) {
+        setCount(count);
+        count--;
+      } else if (count === 1) {
+        setCount(count);
+        count--;
+      } else {
+        clearInterval(intervalId);
+        setCount("Smile!");
+        camera.current && setImage(camera.current.takePhoto());
+      }
+    }, 1000);
+  };
+
   useEffect(() => {
     if (takePhotoTrigger) {
-      camera.current && setImage(camera?.current?.takePhoto());
+      startCountdown();
     }
   }, [takePhotoTrigger]);
 
@@ -46,16 +69,19 @@ const SnapshotForm = ({
 
   return (
     <div>
-      <div className="rounded-md">
-        <Camera
-          ref={camera}
-          aspectRatio={16 / 9}
-          errorMessages={cameraErroMessage}
-        />
-      </div>
-
-      {image && (
-        <Image src={image} alt="Taken photo" width={100} height={100} />
+      {image ? (
+        <div className="relative h-72 w-full">
+          <Image src={image} alt="Taken photo" fill />
+        </div>
+      ) : (
+        <div className="relative flex items-center justify-center rounded-md">
+          <p className="absolute z-20 text-3xl font-bold text-white">{count}</p>
+          <Camera
+            ref={camera}
+            aspectRatio={16 / 9}
+            errorMessages={cameraErroMessage}
+          />
+        </div>
       )}
     </div>
   );
