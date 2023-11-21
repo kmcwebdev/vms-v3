1ffc6cf0366300002a7dac22730fbffefc3696a2
Seth Maño <joshuaseth11@gmail.com>
2023-11-21 09:46:23 +0800

    style: optimize stepper component

--- components/tablet/stepper.tsx
+++ components/tablet/stepper.tsx
@@ -1,10 +1,59 @@
 import React from "react";
-import { Card, CardContent } from "../ui/card";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+} from "../ui/card";
+import { Progress } from "../ui/progress";
+import { Check } from "lucide-react";
+
+interface ISteps {
+  id: number;
+  label: string;
+  field: string;
+}
+const Steps: ISteps[] = [
+  {
+    id: 1,
+    label: "STEP 1",
+    field: "Fill up form",
+  },
+  {
+    id: 2,
+    label: "STEP 2",
+    field: "Take a snapshot",
+  },
+  {
+    id: 1,
+    label: "STEP 3",
+    field: "Review details",
+  },
+];
 
 const Stepper = () => {
   return (
-    <Card className="shadow-none">
-      <CardContent></CardContent>
+    <Card className="px-4 py-4 pt-0 shadow-none">
+      <CardContent className="p-0">
+        <div className="mt-4 flex w-full justify-evenly">
+          {Steps.map((e) => (
+            <Card key={e.id} className="w-full border-none p-2 shadow-none">
+              <CardHeader className="flex flex-row items-center gap-x-3 p-0">
+                <div className="relative flex h-6 w-7 items-center justify-center rounded-full border text-xs">
+                  {/* <p className="m-auto">1</p> */}
+                  <Check className="mx-auto h-3 w-3" />
+                </div>
+
+                <Progress value={100} className=" h-1" />
+              </CardHeader>
+              <CardContent className="mt-2 px-0 py-2 text-sm">
+                <p className="leading-none">{e.field}</p>
+              </CardContent>
+            </Card>
+          ))}
+        </div>
+      </CardContent>
     </Card>
   );
 };
