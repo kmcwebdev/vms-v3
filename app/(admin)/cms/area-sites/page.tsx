65a938aa1c934cefa83746df9c3c358ea4fd22df
Gelo <chrisgelosulit@gmail.com>
2023-11-21 16:36:27 +0800

    feat(area-sites): Add API request to fetch and display sites

--- app/(admin)/cms/area-sites/page.tsx
+++ app/(admin)/cms/area-sites/page.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import AreaSites from "@/components/admin/cms/area-sites/area-sites";
+import { Site } from "@/types/site";
+import { env } from "@/lib/env.mjs";
 
-const page = () => {
-  return <AreaSites />;
+const page = async () => {
+  const response = await fetch(`${env.BASEURL}/api/area-sites/all`);
+
+  const sites = (await response.json()) as Site[];
+
+  return <AreaSites sites={sites} />;
 };
 
 export default page;
--- hooks/useGetAllSites.ts
+++ hooks/useGetAllSites.ts
@@ -9,15 +9,22 @@ const getAllSitesQuery = async ({ filter }: GetAllSitesQuery = {}) => {
   const params = new URLSearchParams({
     filter: filter ? filter : "",
   });
+
   const URL = `/api/area-sites/all${
     filter ? `${"?" + params.toString()}` : ""
   }`;
+
   const response = await fetch(URL);
+
   return (await response.json()) as Site[];
 };
 
-export const useGetAllSites = ({ filter }: GetAllSitesQuery = {}) => {
+export const useGetAllSites = (
+  { filter }: GetAllSitesQuery = {},
+  initialData: Site[],
+) => {
   const { data, isLoading, isFetching, isError } = useQuery({
+    initialData,
     queryKey: ["all-sites", JSON.stringify(filter)],
     queryFn: () =>
       getAllSitesQuery({
