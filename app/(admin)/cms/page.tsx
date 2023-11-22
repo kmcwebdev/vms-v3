fdf68b2e4a823d12893db5e439725f02bfbbe684
Seth Maño <joshuaseth11@gmail.com>
2023-11-22 15:55:47 +0800

    feat(admin): add site prop to Admin component

--- app/(admin)/cms/page.tsx
+++ app/(admin)/cms/page.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import Admin from "@/components/admin";
+import { Site } from "@/types/site";
+import { env } from "@/lib/env.mjs";
 
-const page = () => {
-  return <Admin />;
+const page = async () => {
+  const response = await fetch(`${env.BASEURL}/api/sites/all`);
+
+  const sites = (await response.json()) as Site[];
+
+  return <Admin site={sites} />;
 };
 
 export default page;
