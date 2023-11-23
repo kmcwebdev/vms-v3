804596ef9047d70dac4752bd4d2cb0892b170366
Gelo <chrisgelosulit@gmail.com>
2023-11-23 12:51:10 +0800

    feat(api): add query parameter validation for GET requests

--- app/api/analytics/visitors/count/sites/route.ts
+++ app/api/analytics/visitors/count/sites/route.ts
@@ -1,17 +1,55 @@
 import { NextResponse } from "next/server";
 import { sql } from "@vercel/postgres";
+import { z } from "zod";
 
-export async function GET() {
+export const querySchema = z.object({
+  year: z
+    .preprocess((input) => Number(input), z.number().min(2000))
+    .optional()
+    .default(new Date().getFullYear()),
+  site_ids: z
+    .preprocess(
+      (input) => String(input).split(/\s*,\s*/),
+      z.array(z.string().uuid()),
+    )
+    .optional(),
+});
+
+export async function GET(req: Request) {
   try {
+    const { searchParams } = new URL(req.url);
+
+    const queryParams = querySchema.safeParse(Object.fromEntries(searchParams));
+
+    if (queryParams.success === false) {
+      return new Response(queryParams.error.message, {
+        status: 400,
+        statusText: "Bad request",
+      });
+    }
+
+    let { year, site_ids } = queryParams.data;
+
+    if (!site_ids) {
+      site_ids = (await sql.query("select site_id from sites")).rows.map(
+        (data) => data.site_id,
+      );
+    }
+
     const visitor_count_by_site_query = `
       select s.site_name, count(*) as visitor_count
       from visitor_logs v
       join sites s on v.site_id = s.site_id
+      where
+          extract(year from visit_time) = $1 -- year
+          and site_id in (${site_ids
+            .map((element) => `'${element}'`)
+            .join(",")}) -- sites
       group by s.site_name
       order by visitor_count;
     `;
 
-    const result = await sql.query(visitor_count_by_site_query);
+    const result = await sql.query(visitor_count_by_site_query, [year]);
 
     return NextResponse.json(result.rows);
   } catch (error: any) {
--- app/api/analytics/visitors/count/yearly/route.ts
+++ app/api/analytics/visitors/count/yearly/route.ts
@@ -14,6 +14,7 @@ export const querySchema = z.object({
     )
     .optional(),
 });
+
 export async function GET(req: Request) {
   try {
     const { searchParams } = new URL(req.url);
