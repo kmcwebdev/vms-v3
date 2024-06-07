76962c1d3a30243ff613bbb90fb1fe31badd803c
Aditya Ghosh <adityaghosh2002@gmail.com>
2024-06-07 12:54:57 +0800

    testing page made for dateRange

--- components/admin/cms/permits/forms/gate-pass/steps/part2.tsx
+++ components/admin/cms/permits/forms/gate-pass/steps/part2.tsx
@@ -3,66 +3,148 @@ import DateRangePicker from "@/components/global/date-range-picker";
 import { TimeRangeSelector } from "@/components/ui/time-range-selector";
 import { Textarea } from "@/components/ui/textarea";
 import React from "react";
+import { Button } from "@/components/ui/button";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 
-export default function Part2() {
+export default function Part2({ formControl }: { formControl: any }) {
   const [selectedTimeRange, setSelectedTimeRange] = React.useState({
     start: "08:00",
     end: "17:00",
   });
+  const [selectedCarrierName, setSelectedCarrierName] = React.useState("");
+  const [selectedCompany, setSelectedCompany] = React.useState("");
+  const [selectedDate, setSelectedDate] = React.useState("");
+  const [selectedReason, setSelectedReason] = React.useState("");
+
   return (
     <>
       {/* Carrier Name and Company / Contractor */}
-      <div className="mb-4 flex flex-row justify-evenly gap-5 p-2">
-        <div className="w-full">
-          <label className="block text-sm font-medium text-gray-700">
-            Carrier Name
-          </label>
-          <Input
-            type="text"
-            className="mt-1 block w-full rounded-md border border-gray-300 p-2"
-            required
-            placeholder="Input Personnel Name"
-          />
-        </div>
-        <div className="w-full">
-          <label className="block text-sm font-medium text-gray-700">
-            Company / Contractor
-          </label>
-          <Input
-            type="text"
-            className="mt-1 block w-full rounded-md border border-gray-300 p-2"
-            required
-            placeholder="Input Personnel Company"
-          />
-        </div>
+
+      <div className="flex flex-row justify-evenly gap-5 p-2">
+        <FormField
+          control={formControl.control}
+          name="carrierName"
+          render={({ field }) => {
+            return (
+              <FormItem className="flex w-full flex-col">
+                <FormLabel>Carrier Name</FormLabel>
+                <FormControl>
+                  <Input
+                    type="text"
+                    className="mt-1 block w-full rounded-md border border-gray-300"
+                    placeholder="Input Personnel Name"
+                    onChange={(e) => {
+                      setSelectedCarrierName(e.target.value);
+                      field.onChange(e.target.value);
+                    }}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            );
+          }}
+        />
+        <FormField
+          control={formControl.control}
+          name="company"
+          render={({ field }) => {
+            return (
+              <FormItem className="flex w-full flex-col">
+                <FormLabel>Company / Contractor</FormLabel>
+                <FormControl>
+                  <Input
+                    type="text"
+                    className="mt-1 block w-full rounded-md border border-gray-300"
+                    placeholder="Input Personnel Company"
+                    {...field}
+                    onChange={(e) => {
+                      setSelectedCompany(e.target.value);
+                      field.onChange(e.target.value);
+                    }}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            );
+          }}
+        />
       </div>
 
       {/* Date and Time */}
-      <div className="mb-4 flex flex-row justify-start gap-5 p-2">
-        <div className="w-full">
-          <label className="block text-sm font-medium text-gray-700">
-            Date
-          </label>
-          <DateRangePicker className="border-gray-300" name="dateRange" />
-        </div>
-        <div className="w-full">
-          <label className="block text-sm font-medium text-gray-700">
-            Time Range
-          </label>
-          <TimeRangeSelector
-            value={selectedTimeRange}
-            onChange={setSelectedTimeRange}
-          />
-        </div>
+      <div className="flex flex-row justify-start gap-5 p-2">
+        <FormField
+          control={formControl.control}
+          name="dateRange"
+          render={({ field }) => {
+            return (
+              <FormItem className="flex w-full flex-col">
+                <FormLabel>Date</FormLabel>
+                <FormControl>
+                  <DateRangePicker
+                    {...field}
+                    className="border-gray-300"
+                    name="dateRange"
+                    onChange={(e) => {
+                      console.log(e)
+                    }}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            );
+          }}
+        />
+        {/* <FormField
+          control={formControl.control}
+          name="time"
+          render={({ field }) => {
+            return (
+              <FormItem className="flex w-full flex-col">
+                <FormLabel>Time</FormLabel>
+                <FormControl>
+                  <TimeRangeSelector
+                    {...field}
+                    value={selectedTimeRange}
+                    onChange={setSelectedTimeRange}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            );
+          }}
+        /> */}
       </div>
 
       {/* Reason for Visit */}
-      <div className="mb-4 gap-5 p-2">
-        <label className="block text-sm font-medium text-gray-700">
-          Reason
-        </label>
-        <Textarea placeholder="Input your Reason" className="border-gray-300" />
-      </div>
+      <FormField
+        control={formControl.control}
+        name="reason"
+        render={({ field }) => {
+          return (
+            <FormItem className="flex w-full flex-col p-2">
+              <FormLabel>Reason</FormLabel>
+              <FormControl>
+                <Textarea
+                  {...field}
+                  placeholder="Input your Reason"
+                  className="border-gray-300"
+                  onChange={(e) => {
+                    setSelectedReason(e.target.value);
+                    field.onChange(e.target.value);
+                  }}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          );
+        }}
+      />
     </>
   );
 }
--- schema/gate-pass.ts
+++ schema/gate-pass.ts
@@ -1,4 +1,5 @@
-import z from "zod";``
+import z from "zod";
+
 
 const itemSchema = z.object({
   description: z.string().optional(),
@@ -9,41 +10,35 @@ const itemSchema = z.object({
 
 const timeRegex = /^([01]\d|2[0-3]):?([0-5]\d)$/;
 
-const fillUpFormSchema = z.object({
+const gatePassSchema = z.object({
   type: z.string(),
-  email: z
-    .string()
-    .min(1, { message: "This field has to be filled." })
-    .email("This is not a valid email."),
+  email: z.string().email(),
   name: z.string().min(1, "This field has to be filled."),
-  serviceCategory: z
-    .string()
-    .min(1, { message: "This field has to be filled." }),
+  serviceCategory: z.string(),
   site: z.string().min(1, { message: "This field has to be filled." }),
   floor: z.string().min(1, { message: "This field has to be filled." }),
   carrierName: z.string().min(1, { message: "This field has to be filled." }),
   company: z.string().min(1, { message: "This field has to be filled." }),
-  date: z.date(),
-  startTime: z.string().refine(val => timeRegex.test(val), {
-    message: "Invalid time format. Expected HH:MM."
-  }),
-  endTime: z.string().refine(val => timeRegex.test(val), {
-    message: "Invalid time format. Expected HH:MM."
-  }),
+  dateRange: z.string(),
+  // startDate: z.date(),
+  // endDate: z.date(),
+  // startTime: z.string().refine(val => timeRegex.test(val), {
+  //   message: "Invalid time format. Expected HH:MM."
+  // }),
+  // endTime: z.string().refine(val => timeRegex.test(val), {
+  //   message: "Invalid time format. Expected HH:MM."
+  // }),
   reason: z.string().optional(),
-  emailsToNotify: z
-    .array(
-      z
-        .string()
-        .min(1, { message: "This field has to be filled." })
-        .email("This is not a valid email.")
-        .optional(),
-    )
-    .optional(),
-  items: z.array(itemSchema).optional(),
-  files: z.array(z.instanceof(File)).optional(),
+  // emailsToNotify: z
+  // .array(
+  //   z
+  //     .string()
+  //     .email("This is not a valid email.")
+  // )
+  // .optional(),
+  // items: z.array(itemSchema).optional(),
+  // files: z.array(z.instanceof(File)).optional(),
 });
 
-export const gatePassSchema = z.object({
-  fillUpForm: fillUpFormSchema,
-});
+export {gatePassSchema}
+
